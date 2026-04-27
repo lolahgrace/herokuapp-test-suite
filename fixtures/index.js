404e628d21d29fdaff0e94befea27330a346d514
Ololade Adekunle <adekunleololade6@gmail.com>
2026-04-27 16:35:16 +0100

    feat: simplify loggedInPage fixture and add afterEach cleanup

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,16 +1,19 @@
-import{test as base} from "@playwright/test";
+import { test as base } from "@playwright/test";
 import LoginPage from '../pages/LoginPage.js'
 
 export const test = base.extend({
-    loggedInPage: async ({ page}, use) => {
+    loggedInPage: async ({ page }, use) => {
+        // Log in via UI
+        const loginPage = new LoginPage(page)
+        await loginPage.goto()
+        await loginPage.login(
+            process.env.LOGIN_USERNAME,
+            process.env.LOGIN_PASSWORD
+        )
 
-        const loginPage = new LoginPage(page);
-        await loginPage.goto();
-        await loginPage.login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD)
-
-        await use(loginPage);
-
+        // Hand off logged-in LoginPage to test
+        await use(loginPage)
     }
 })
 
-export {expect} from '@playwright/test'
+export { expect } from '@playwright/test'
